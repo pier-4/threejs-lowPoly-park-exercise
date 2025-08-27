5b470935e9a7e530c9402a78e2477c4f87e4b967
pier-4 <pier.dzn44@gmail.com>
2025-08-27 19:11:59 +0200

    fix deployment error

--- src/main.js
+++ src/main.js
@@ -160,7 +160,7 @@ const intersectObjectsNames = ["Project1", "Project2", "Project3"];
 let model;
 // Load .glb
 loader.load(
-  "/portfolio-with-collider.glb",
+  "/Portfolio-with-collider.glb",
   (glb) => {
     model = glb.scene;
     model.traverse((child) => {
